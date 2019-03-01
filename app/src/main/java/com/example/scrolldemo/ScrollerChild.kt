cc78b38b06e3db70a4f7a560b40fa5dc4b1ed6f6
yupenglei <yupengniaosanshi@gmail.com>
2019-03-01 13:58:08 +0800

    对方法添加简单注释，并排序

--- app/src/main/java/com/example/scrolldemo/ScrollerChild.kt
+++ app/src/main/java/com/example/scrolldemo/ScrollerChild.kt
@@ -2,13 +2,77 @@ package com.example.scrolldemo
 
 import android.content.Context
 import android.support.v4.view.NestedScrollingChild2
+import android.support.v4.view.NestedScrollingChildHelper
 import android.util.AttributeSet
+import android.util.Log
 import android.view.View
 
 /**
  * @author penglei on 2019/3/1
  */
 class ScrollerChild : View, NestedScrollingChild2 {
+    companion object {
+        private val TAG = ScrollerChild::class.java.simpleName
+    }
+
+    private val helper = NestedScrollingChildHelper(this)
+
+    /**
+     * 是否有可滚动的父View
+     */
+    override fun hasNestedScrollingParent(type: Int): Boolean {
+        return helper.hasNestedScrollingParent(type)
+    }
+
+    override fun hasNestedScrollingParent(): Boolean {
+        return helper.hasNestedScrollingParent()
+    }
+
+    /**
+     * 设置是否可滚动
+     */
+    override fun setNestedScrollingEnabled(enabled: Boolean) {
+        helper.isNestedScrollingEnabled = enabled
+    }
+
+    /**
+     * 是否可滚动
+     */
+    override fun isNestedScrollingEnabled(): Boolean {
+        return helper.isNestedScrollingEnabled
+    }
+
+    /**
+     * 本View分发预滚动
+     */
+    override fun dispatchNestedPreScroll(
+        dx: Int,
+        dy: Int,
+        consumed: IntArray?,
+        offsetInWindow: IntArray?,
+        type: Int
+    ): Boolean {
+        return helper.dispatchNestedPreScroll(dx, dy, consumed, offsetInWindow, type)
+    }
+
+    override fun dispatchNestedPreScroll(dx: Int, dy: Int, consumed: IntArray?, offsetInWindow: IntArray?): Boolean {
+        return helper.dispatchNestedPreScroll(dx, dy, consumed, offsetInWindow)
+    }
+
+    /**
+     * 本View开始滚动
+     */
+    override fun startNestedScroll(axes: Int, type: Int): Boolean {
+        return helper.startNestedScroll(axes, type)
+    }
+
+    override fun startNestedScroll(axes: Int): Boolean {
+        return helper.startNestedScroll(axes)
+    }
+
+    /**
+     * 本View分发已滚动
+     */
     override fun dispatchNestedScroll(
         dxConsumed: Int,
         dyConsumed: Int,
@@ -17,7 +81,7 @@ class ScrollerChild : View, NestedScrollingChild2 {
         offsetInWindow: IntArray?,
         type: Int
     ): Boolean {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return helper.dispatchNestedScroll(dxConsumed, dyConsumed, dxUnconsumed, dyUnconsumed, offsetInWindow, type)
     }
 
     override fun dispatchNestedScroll(
@@ -27,61 +91,29 @@ class ScrollerChild : View, NestedScrollingChild2 {
         dyUnconsumed: Int,
         offsetInWindow: IntArray?
     ): Boolean {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return helper.dispatchNestedScroll(dxConsumed, dyConsumed, dxUnconsumed, dyUnconsumed, offsetInWindow)
     }
 
-    override fun isNestedScrollingEnabled(): Boolean {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun dispatchNestedPreScroll(
-        dx: Int,
-        dy: Int,
-        consumed: IntArray?,
-        offsetInWindow: IntArray?,
-        type: Int
-    ): Boolean {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun dispatchNestedPreScroll(dx: Int, dy: Int, consumed: IntArray?, offsetInWindow: IntArray?): Boolean {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
+    /**
+     * 本View停止滚动
+     */
     override fun stopNestedScroll(type: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return helper.stopNestedScroll(type)
     }
 
     override fun stopNestedScroll() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return helper.stopNestedScroll()
     }
 
-    override fun hasNestedScrollingParent(type: Int): Boolean {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun hasNestedScrollingParent(): Boolean {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
+    /**
+     * 滑行相关
+     */
     override fun dispatchNestedPreFling(velocityX: Float, velocityY: Float): Boolean {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun setNestedScrollingEnabled(enabled: Boolean) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return helper.dispatchNestedPreFling(velocityX, velocityY)
     }
 
     override fun dispatchNestedFling(velocityX: Float, velocityY: Float, consumed: Boolean): Boolean {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun startNestedScroll(axes: Int, type: Int): Boolean {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun startNestedScroll(axes: Int): Boolean {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return helper.dispatchNestedFling(velocityX, velocityY, consumed)
     }
 
     // ===================
@@ -89,4 +121,8 @@ class ScrollerChild : View, NestedScrollingChild2 {
 
     constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs)
     constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
+
+    private fun log(msg: String) {
+        Log.d(">>> $TAG", msg)
+    }
 }
--- app/src/main/java/com/example/scrolldemo/ScrollerParent.kt
+++ app/src/main/java/com/example/scrolldemo/ScrollerParent.kt
@@ -1,9 +1,9 @@
 package com.example.scrolldemo
 
 import android.content.Context
-import android.support.v4.view.NestedScrollingChild2
-import android.support.v4.view.NestedScrollingParent2
+import android.support.v4.view.*
 import android.util.AttributeSet
+import android.util.Log
 import android.view.View
 import android.view.ViewGroup
 
@@ -11,7 +11,71 @@ import android.view.ViewGroup
  * @author penglei on 2019/3/1
  */
 class ScrollerParent : ViewGroup, NestedScrollingParent2, NestedScrollingChild2 {
-    // ========================
+    companion object {
+        private val TAG = ScrollerParent::class.java.simpleName
+    }
+
+    private val parentHelper = NestedScrollingParentHelper(this)
+    private val childHelper = NestedScrollingChildHelper(this)
+
+    // ========================Child========================
+
+    /**
+     * 是否有可滚动的父View
+     */
+    override fun hasNestedScrollingParent(type: Int): Boolean {
+        return childHelper.hasNestedScrollingParent(type)
+    }
+
+    override fun hasNestedScrollingParent(): Boolean {
+        return childHelper.hasNestedScrollingParent()
+    }
+
+    /**
+     * 设置是否可滚动
+     */
+    override fun setNestedScrollingEnabled(enabled: Boolean) {
+        childHelper.isNestedScrollingEnabled = enabled
+    }
+
+    /**
+     * 是否可滚动
+     */
+    override fun isNestedScrollingEnabled(): Boolean {
+        return childHelper.isNestedScrollingEnabled
+    }
+
+    /**
+     * 本View分发预滚动
+     */
+    override fun dispatchNestedPreScroll(
+        dx: Int,
+        dy: Int,
+        consumed: IntArray?,
+        offsetInWindow: IntArray?,
+        type: Int
+    ): Boolean {
+        return childHelper.dispatchNestedPreScroll(dx, dy, consumed, offsetInWindow, type)
+    }
+
+    override fun dispatchNestedPreScroll(dx: Int, dy: Int, consumed: IntArray?, offsetInWindow: IntArray?): Boolean {
+        return childHelper.dispatchNestedPreScroll(dx, dy, consumed, offsetInWindow)
+    }
+
+    /**
+     * 本View开始滚动
+     */
+    override fun startNestedScroll(axes: Int, type: Int): Boolean {
+        return childHelper.startNestedScroll(axes, type)
+    }
+
+    override fun startNestedScroll(axes: Int): Boolean {
+        return childHelper.startNestedScroll(axes)
+    }
+
+    /**
+     * 本View分发已滚动
+     */
     override fun dispatchNestedScroll(
         dxConsumed: Int,
         dyConsumed: Int,
@@ -20,7 +84,7 @@ class ScrollerParent : ViewGroup, NestedScrollingParent2, NestedScrollingChild2 
         offsetInWindow: IntArray?,
         type: Int
     ): Boolean {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return childHelper.dispatchNestedScroll(dxConsumed, dyConsumed, dxUnconsumed, dyUnconsumed, offsetInWindow, type)
     }
 
     override fun dispatchNestedScroll(
@@ -30,96 +94,78 @@ class ScrollerParent : ViewGroup, NestedScrollingParent2, NestedScrollingChild2 
         dyUnconsumed: Int,
         offsetInWindow: IntArray?
     ): Boolean {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return childHelper.dispatchNestedScroll(dxConsumed, dyConsumed, dxUnconsumed, dyUnconsumed, offsetInWindow)
     }
 
-    override fun isNestedScrollingEnabled(): Boolean {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun dispatchNestedPreScroll(
-        dx: Int,
-        dy: Int,
-        consumed: IntArray?,
-        offsetInWindow: IntArray?,
-        type: Int
-    ): Boolean {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun dispatchNestedPreScroll(dx: Int, dy: Int, consumed: IntArray?, offsetInWindow: IntArray?): Boolean {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
+    /**
+     * 本View停止滚动
+     */
     override fun stopNestedScroll(type: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return childHelper.stopNestedScroll(type)
     }
 
     override fun stopNestedScroll() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return childHelper.stopNestedScroll()
     }
 
-    override fun hasNestedScrollingParent(type: Int): Boolean {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun hasNestedScrollingParent(): Boolean {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
+    /**
+     * 滑行相关
+     */
     override fun dispatchNestedPreFling(velocityX: Float, velocityY: Float): Boolean {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun setNestedScrollingEnabled(enabled: Boolean) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return childHelper.dispatchNestedPreFling(velocityX, velocityY)
     }
 
     override fun dispatchNestedFling(velocityX: Float, velocityY: Float, consumed: Boolean): Boolean {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return childHelper.dispatchNestedFling(velocityX, velocityY, consumed)
     }
 
-    override fun startNestedScroll(axes: Int, type: Int): Boolean {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+    // ==============================Parent==============================
 
-    override fun startNestedScroll(axes: Int): Boolean {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    // ==============================
-    override fun onNestedScrollAccepted(child: View, target: View, axes: Int, type: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun onNestedScrollAccepted(child: View, target: View, axes: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
+    /**
+     * @return true表示允许子View嵌套滚动
+     */
     override fun onStartNestedScroll(child: View, target: View, axes: Int, type: Int): Boolean {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        // 允许滑动
+        return true
     }
 
     override fun onStartNestedScroll(child: View, target: View, axes: Int): Boolean {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return onStartNestedScroll(child, target, axes, ViewCompat.TYPE_TOUCH)
     }
 
-    override fun onNestedFling(target: View, velocityX: Float, velocityY: Float, consumed: Boolean): Boolean {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    /**
+     * 在子View进行滚动前的资源初始化
+     * [onStartNestedScroll]返回true，后调用
+     */
+    override fun onNestedScrollAccepted(child: View, target: View, axes: Int, type: Int) {
+        return parentHelper.onNestedScrollAccepted(child, target, axes, type)
     }
 
+    override fun onNestedScrollAccepted(child: View, target: View, axes: Int) {
+        return parentHelper.onNestedScrollAccepted(child, target, axes)
+    }
+
+    /**
+     * 其值由[onNestedScrollAccepted]设置
+     */
     override fun getNestedScrollAxes(): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return parentHelper.nestedScrollAxes
     }
 
+    /**
+     * 在子View滚动前，可能本View需要滚动或其他操作
+     */
     override fun onNestedPreScroll(target: View, dx: Int, dy: Int, consumed: IntArray?, type: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        // 不进行预消耗
     }
 
     override fun onNestedPreScroll(target: View, dx: Int, dy: Int, consumed: IntArray) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        onNestedPreScroll(target, dx, dy, consumed, ViewCompat.TYPE_TOUCH)
     }
 
+    /**
+     * 滚动过后，分析子View消耗和未消耗的距离，进行某些操作
+     */
     override fun onNestedScroll(
         target: View,
         dxConsumed: Int,
@@ -128,23 +174,35 @@ class ScrollerParent : ViewGroup, NestedScrollingParent2, NestedScrollingChild2 
         dyUnconsumed: Int,
         type: Int
     ) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        // do nothing
     }
 
     override fun onNestedScroll(target: View, dxConsumed: Int, dyConsumed: Int, dxUnconsumed: Int, dyUnconsumed: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        onNestedScroll(target, dxConsumed, dyConsumed, dxUnconsumed, dyUnconsumed, ViewCompat.TYPE_TOUCH)
     }
 
-    override fun onNestedPreFling(target: View, velocityX: Float, velocityY: Float): Boolean {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
+    /**
+     * 子View滚动停止时
+     */
     override fun onStopNestedScroll(target: View, type: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        parentHelper.onStopNestedScroll(target, type)
     }
 
     override fun onStopNestedScroll(target: View) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        parentHelper.onStopNestedScroll(target)
+    }
+
+    /**
+     * 滑行相关
+     */
+    override fun onNestedPreFling(target: View, velocityX: Float, velocityY: Float): Boolean {
+        // 允许滑行
+        return true
+    }
+
+    override fun onNestedFling(target: View, velocityX: Float, velocityY: Float, consumed: Boolean): Boolean {
+        // 不拦截滑动
+        return false
     }
     // ============================
 
@@ -156,4 +214,8 @@ class ScrollerParent : ViewGroup, NestedScrollingParent2, NestedScrollingChild2 
     constructor(context: Context?) : super(context)
     constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs)
     constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
+
+    private fun log(msg: String) {
+        Log.d(">>> $TAG", msg)
+    }
 }
